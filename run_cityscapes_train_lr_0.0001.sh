76ad9f2e927b3e813f614dff7bbd3225a20a7d5d
Yufei HU <mhu6729@gmail.com>
2021-12-21 10:27:24 +0100

    update

---
+++ run_cityscapes_train_lr_0.0001.sh
@@ -0,0 +1,11 @@
+CUDA_VISIBLE_DEVICES=0,1  python3 main.py --data_root "/home/student/workspace_Yufei/CityScapes/leftImg8bit/"\
+                        --dataset "cityscapes" \
+                        --model "deeplabv3plus_resnet50"\
+                        --output_stride 8 \
+                        --batch_size 12 \
+                        --crop_size 768 \
+                        --gpu_id 0,1 \
+                        --lr 0.1 \
+			> cityscapes_best_obow_resnet_imagenet_depth_train_extra_p9.out
+#                        --ckpt
+			#--ckpt './pre_models/model_best_rms_7.55890_depth_deeplab_best_model_p11.pth'\
---
+++ run_cityscapes_valid.sh
@@ -0,0 +1,13 @@
+CUDA_VISIBLE_DEVICES=0  python3 valid.py --data_root "/home/student/workspace_Yufei/CityScapes/leftImg8bit/"\
+                        --dataset "cityscapes" \
+                        --model "deeplabv3plus_resnet50"\
+                        --output_stride 8 \
+                        --batch_size 12 \
+                        --crop_size 68 \
+                        --gpu_id 0 \
+                        --lr 0.1 \
+			--test_only \
+			--ckpt './checkpoints_obow_deeplabv3plus_s15_PC/best_deeplabv3plus_resnet50_cityscapes_os8.pth' \
+			--val_batch_size 1\
+                        > cityscapes_depth_valid.out
+#                        --ckpt
